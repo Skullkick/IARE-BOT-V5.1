2be0cb8f5e5f37bdfb8b6f231720c48b51bfb3b5
Skullkick <121807883+Skullkick@users.noreply.github.com>
2024-03-11 14:12:04 +0530

    Added all the required files of IARE BOT V5.1
    Contains the main file which contains the code to start the bot, a database folder containing tdatabase file which contains the local sqllite3 code.

---
+++ DATABASE/tdatabase.py
@@ -0,0 +1,161 @@
+#This file is used to store userdata temporarily in local storage
+import sqlite3, json
+from pytz import timezone
+from datetime import datetime
+
+DATABASE_FILE = "user_sessions.db"
+
+TOTAL_USERS_DATABASE_FILE = "total_users.db"
+
+REQUESTS_DATABASE_FILE = "requests.db"
+
+async def get_indian_time():
+    return datetime.now(timezone('Asia/Kolkata'))
+async def create_tables():
+    """
+    Create the necessary tables in the SQLite database.
+    """
+    with sqlite3.connect(DATABASE_FILE) as conn:
+        cursor = conn.cursor()
+        # Create a table to store user sessions
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS sessions (
+                chat_id INTEGER PRIMARY KEY,
+                session_data TEXT,
+                last_activity TIMESTAMP,
+                user_id TEXT
+            )
+        """)
+        conn.commit()
+#The usernames accessed this bot
+async def create_total_users_table():
+    """
+    Create the total_users table in the SQLite database.
+    """
+    with sqlite3.connect(TOTAL_USERS_DATABASE_FILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY,
+                username TEXT UNIQUE
+            )
+        """)
+        conn.commit()
+
+async def store_user_session(chat_id, session_data, user_id):
+    """
+    Store the user session data in the SQLite database.
+
+    Parameters:
+        chat_id (int): The chat ID of the user.
+        session_data (str): JSON-formatted string containing the session data.
+    """
+    with sqlite3.connect(DATABASE_FILE) as conn:
+        cursor = conn.cursor()
+
+        current_time = await get_indian_time()
+
+        cursor.execute("INSERT OR REPLACE INTO sessions (chat_id, session_data, last_activity, user_id) VALUES (?, ?, ?, ?)",
+                       (chat_id, session_data, current_time, user_id))
+        
+        conn.commit()
+
+async def store_username(username):
+    """
+    Store a username in the total_users table.
+
+    Parameters:
+        username (str): The username to store.
+    """
+    with sqlite3.connect(TOTAL_USERS_DATABASE_FILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute("INSERT OR IGNORE INTO users (username) VALUES (?)",
+                       (username,))
+        conn.commit()
+
+async def load_user_session(chat_id):
+    with sqlite3.connect(DATABASE_FILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT session_data FROM sessions WHERE chat_id=?", (chat_id,))
+        result = cursor.fetchone()
+        if result:
+            session_data = json.loads(result[0])
+            # Check if the session data contains the 'username'
+            if 'username' in session_data:
+                return session_data
+    return None
+
+async def load_username(chat_id):
+    with sqlite3.connect(DATABASE_FILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM sessions WHERE chat_id=?", (chat_id,))
+        row = cursor.fetchone()
+        return row
+
+async def delete_user_session(chat_id):
+    """
+    Delete the user session data from the SQLite database.
+
+    Parameters:
+        chat_id (int): The chat ID of the user.
+    """
+    with sqlite3.connect(DATABASE_FILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM sessions WHERE chat_id=?", (chat_id,))
+        conn.commit()
+
+async def clear_sessions_table():
+    """
+    Clear all rows from the sessions table.
+    """
+    with sqlite3.connect(DATABASE_FILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM sessions")
+        conn.commit()
+
+async def create_requests_table():
+    with sqlite3.connect(REQUESTS_DATABASE_FILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS pendingreq(
+                       unique_id TEXT PRIMARY KEY,
+                       user_id TEXT,
+                       message TEXT,
+                       chat_id INTEGER
+            )
+        """)
+        conn.commit()
+
+async def store_requests(unique_id, user_id, message, chat_id):
+    with sqlite3.connect(REQUESTS_DATABASE_FILE) as conn:
+        c = conn.cursor()
+        c.execute("INSERT INTO pendingreq (unique_id, user_id, message, chat_id) VALUES (?, ?, ?, ?)",
+                  (unique_id, user_id, message, chat_id))
+        conn.commit()
+
+async def load_requests(unique_id):
+    with sqlite3.connect(REQUESTS_DATABASE_FILE ) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM pendingreq WHERE unique_id=?",(unique_id,))
+        row = cursor.fetchone()
+        return row
+    
+async def load_allrequests():
+    with sqlite3.connect(REQUESTS_DATABASE_FILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM pendingreq")
+        all_messages = cursor.fetchall()
+        return all_messages
+
+async def delete_request(unique_id):
+    with sqlite3.connect(REQUESTS_DATABASE_FILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM pendingreq WHERE unique_id=?",(unique_id,))
+
+async def clear_requests():
+    with sqlite3.connect(REQUESTS_DATABASE_FILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM pendingreq")
+  
+
+
